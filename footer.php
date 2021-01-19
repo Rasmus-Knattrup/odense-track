9e2dfeb953cb8a0c10da798ba1425d1b35f88df9
Rasmus Kjems Knattrup <rasm355k@tietgen.dk>
2021-01-19 13:37:12 +0100

    [feat] footer (WIP)

--- footer.php
+++ footer.php
@@ -1,4 +1,12 @@
 
     </div>
+    <footer class="footer">
+        <div class="footer-wrapper">
+            <div class="logo">
+                <p class="logo-odense">ODENSE<span class="logo-track">TRACK</span></p>
+                <span class="logo-r-and-e">Racing & Events</span>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
