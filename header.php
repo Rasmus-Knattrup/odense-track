939da10f47debe0530953b8b99fea74dd4a91b07
Rasmus Kjems Knattrup <rasm355k@tietgen.dk>
2021-01-19 11:11:08 +0100

    [feat] Mappe struktur + HTML

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/base.css">
+    <link rel="stylesheet" href="css/layout.css">
+    <link rel="stylesheet" href="css/module.css">
+    <link rel="stylesheet" href="css/state.css">
+    <link rel="stylesheet" href="css/theme.css">
+    <title>Odense Track</title>
+</head>
+<body>
+<div class="wrapper">
+    <header>
+
+    </header>
--- index.php
+++ index.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Odense Track</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php include_once 'header.php'; ?>
+
+
+
+<?php include_once 'footer.php'; ?>
