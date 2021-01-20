93973ccd4d4a27584e6eb394b0459945b27a63f3
Rasmus Kjems Knattrup <rasm355k@tietgen.dk>
2021-01-20 11:36:13 +0100

    [feat] footer logo

--- footer.php
+++ footer.php
@@ -2,11 +2,13 @@
     </div>
     <footer class="footer">
         <div class="footer-wrapper">
+
             <div class="logo">
-                <p class="logo-odense">ODENSE</p>
+                <span class="logo-odense">ODENSE</span>
                 <span class="logo-r-and-e">Racing & Events</span>
                 <span class="logo-track">TRACK</span>
             </div>
+            
         </div>
     </footer>
 </body>
