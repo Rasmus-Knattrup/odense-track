f849caeb598ae019bb2ac2507102196193d750e1
Rasmus Kjems Knattrup <rasm355k@tietgen.dk>
2021-01-22 11:26:48 +0100

    [feat] events header CSS + slide counter CSS

--- index.php
+++ index.php
@@ -2,6 +2,18 @@
 
 <main class="main">
     <img class="hero" src="img/hero.png" alt="">
+    <section>
+        <div class="events-header">
+           <h1 class="events-title">EVENTS</h1>
+           <div class="events-nav">
+                <span class="events-counter">
+                    <span class="padding-1 padding-left-8">1</span>af<span class="padding-1">2</span>
+                </span>
+                <button class="events-arrow events-arrow-left"> < </button>
+                <button class="events-arrow events-arrow-right"> > </button>
+           </div>
+        </div>
+    </section>
 </main>
 
 <?php include_once 'footer.php'; ?>
