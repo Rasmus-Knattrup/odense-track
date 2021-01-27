526913edb34601a294141b768e705d1ee20e2b18
Rasmus Kjems Knattrup <rasm355k@tietgen.dk>
2021-01-27 13:51:50 +0100

    [feat] Nyheder (Event center) CSS (WIP)

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
     <img class="hero" src="img/hero.png" alt="">
     <section class="events">
         <div class="events-header">
-           <h1 class="events-title">EVENTS</h1>
-           <div class="events-nav">
+            <h1 class="events-title">EVENTS</h1>
+            <div class="events-nav">
                 <span class="events-counter">1 af 2</span>
                 <button class="events-arrow events-arrow-left"> <i class="arrow left"></i> </button>
                 <button class="events-arrow events-arrow-right"> <i class="arrow right"></i> </button>
-           </div>
+            </div>
         </div>
 
         <div class="events-main">
@@ -50,67 +50,69 @@
                     <button class="events-arrow news-arrow-up"> <i class="arrow up"></i> </button>
                 </div>
             </div>
-            <div class="news-latest-article-wrapper">
+            <div class="news-article-wrapper">
                 <div class="news-latest-date-wrapper">
                     <div class="news-latest-date">
                         <span class="news-latest-day">11</span>
                         <span class="news-latest-month">MAR</span>
                     </div>
                 </div>
-                <article class="news-latest-article">
-                    <div class="news-latest-article-content">
-                        <h3 class="news-latest-article-title">Need For Speed</h3>
-                        <p class="news-latest-article-text">
-                            Kom ud på banen og slip den indre fartdjævel 
+                <article class="news-article">
+                    <div class="news-article-content">
+                        <h3 class="news-article-title">Need For Speed</h3>
+                        <p class="news-article-text">
+                            Kom ud på banen og slip den indre fartdjævel
                             løs. Denne dag er for dig som trænger til at give
                             den gas, og teste din bil på en bane i sikre...
                         </p>
                     </div>
                     <div class="news-read_more-wrapper">
                         <a class="news-latest-article-read_more" href="#">LÆS MERE</a>
-                    </div>   
+                    </div>
                 </article>
             </div>
-            <div class="news-latest-article-wrapper">
+
+            <!-- VVV TO BE DELETED VVV -->
+            <div class="news-article-wrapper">
                 <div class="news-latest-date-wrapper">
                     <div class="news-latest-date">
                         <span class="news-latest-day">12</span>
                         <span class="news-latest-month">MAR</span>
                     </div>
                 </div>
-                <article class="news-latest-article">
-                    <div class="news-latest-article-content">
-                        <h3 class="news-latest-article-title">Motion For Alle</h3>
-                        <p class="news-latest-article-text">
-                            Hver tirsdag tilbyder Odense Track en unik og 
+                <article class="news-article">
+                    <div class="news-article-content">
+                        <h3 class="news-article-title">Motion For Alle</h3>
+                        <p class="news-article-text">
+                            Hver tirsdag tilbyder Odense Track en unik og
                             sikker træningsaften, for cyklister og motionis-
                             ter. Vi kalder denne aften for “Banen Rundt”...
                         </p>
                     </div>
                     <div class="news-read_more-wrapper">
                         <a class="news-latest-article-read_more" href="#">LÆS MERE</a>
-                    </div>   
+                    </div>
                 </article>
             </div>
-            <div class="news-latest-article-wrapper">
+            <div class="news-article-wrapper">
                 <div class="news-latest-date-wrapper">
                     <div class="news-latest-date">
                         <span class="news-latest-day">13</span>
                         <span class="news-latest-month">MAR</span>
                     </div>
                 </div>
-                <article class="news-latest-article">
-                    <div class="news-latest-article-content">
-                        <h3 class="news-latest-article-title">DM i Gokart</h3>
-                        <p class="news-latest-article-text">
-                            Danmarksmesterskaber i Gokart, afholdes i år, 
-                            på Odense Track. Deltagerne kommer fra hele 
+                <article class="news-article">
+                    <div class="news-article-content">
+                        <h3 class="news-article-title">DM i Gokart</h3>
+                        <p class="news-article-text">
+                            Danmarksmesterskaber i Gokart, afholdes i år,
+                            på Odense Track. Deltagerne kommer fra hele
                             landet, niveauet er helt i top, når kørerne...
                         </p>
                     </div>
                     <div class="news-read_more-wrapper">
                         <a class="news-latest-article-read_more" href="#">LÆS MERE</a>
-                    </div>   
+                    </div>
                 </article>
             </div>
         </div>
@@ -118,7 +120,24 @@
         <div class="news-center">
             <h2 class="news-title">EVENT CENTER</h2>
             <div class="news-center-article">
-
+                <div class="news-center-article-wrapper">
+                    <article class="news-article">
+                        <div>
+                            <img src="img/book-evenet-center.png" alt="Placer holder">
+                        </div>
+                        <div class="news-article-content">
+                            <h3 class="news-article-title">Book vores Event Center</h3>
+                            <p class="news-article-text">
+                                Skal du holde firmafest, fødselsdag, reception
+                                eller andet, har vores Event Center et lokale 
+                                som passer til dit behov. Fra 200 - 5000 gæster.
+                            </p>
+                        </div>
+                        <div class="news-center-arrow-wrapper">
+                            <i class="news-center-arrow right"></i>
+                        </div>
+                    </article>
+                </div>
             </div>
         </div>
     </section>
