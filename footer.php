69fd7aaa0230b7ba4403802fa6c5969ad979090b
Rasmus Kjems Knattrup <rasm355k@tietgen.dk>
2021-01-28 13:40:38 +0100

    [feat] footer CSS

--- footer.php
+++ footer.php
@@ -2,13 +2,61 @@
     </div>
     <footer class="footer">
         <div class="footer-wrapper">
+            <div class="footer-main">
+                <div class="footer-logo-frame">
+                    <div class="logo">
+                        <span class="logo-odense">ODENSE</span>
+                        <span class="logo-r-and-e">Racing & Events</span>
+                        <span class="logo-track">TRACK</span>
+                    </div>
+                </div>
+                
 
-            <div class="logo">
-                <span class="logo-odense">ODENSE</span>
-                <span class="logo-r-and-e">Racing & Events</span>
-                <span class="logo-track">TRACK</span>
+                <div class="social-media">
+                    <div class="social-media-content">
+                        <h3 class="footer-main-title">FØLG OS HER</h3>
+                        <span class="footer-main-text">Tilmeld vores sociale netværk</span>
+                        <div>
+                            <img src="img/socials/facebook.png" alt="facebook.png">
+                            <img src="img/socials/twitter.png" alt="twitter.png">
+                            <img src="img/socials/youtube.png" alt="youtube.png">
+                            <img src="img/socials/instagram.png" alt="instagram.png">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="newsletter">
+                    <div class="newsletter-content">
+                        <h3 class="footer-main-title">TILMELD DIG VORES NYHEDSBREV</h3>
+                        <span class="footer-main-text">Tilmeld vores sociale netværk</span>
+                        <form class="insert-email-form">
+                            <input class="insert-email" type="text" placeholder="Din email adresse her">
+                            <button class="insert-email-submit"><i class="arrow right"></i></button>
+                        </form>
+                    </div>
+                </div>
             </div>
-            
+
+            <nav class="footer-nav">
+                <ul class="footer-nav-ul">
+                    <li class="footer-nav-item">
+                        <a class="footer-nav-link" href="#">Kontakt</a>
+                    </li>
+
+                    <span class="line">|</span>
+
+                    <li class="footer-nav-item">
+                        <a class="footer-nav-link" href="#">Om Odense Track</a>
+                    </li>
+
+                    <span class="line">|</span>
+
+                    <li class="footer-nav-item">
+                        <a class="footer-nav-link" href="#">Sitemap</a>
+                    </li>
+                </ul>
+                <span class="footer-nav-copyright">&copy; <span class="footer-nav-label">ODENSE TRACK</span></span>
+            </nav>
         </div>
     </footer>
 </body>
