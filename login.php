ee9130a07a4fb5792070a057eb6c689836c90ff2
Rasmus Kjems Knattrup <rasm355k@tietgen.dk>
2021-02-03 13:09:55 +0100

    [feat] Login system error handling CSS

--- login.php
+++ login.php
@@ -8,32 +8,42 @@
 <!-- LOGIN MAIN -->
 <main class="main">
     <section class="login">
-        <form class="login-form" action="" method="post">
-            <label class="label" for="username">Brugernavn:</label>
-            <input class="login-input" type="text" name="username">
+        <div class="login-wrapper">
+            <form class="login-form" action="" method="post">
+                <label class="label" for="username">Brugernavn:</label>
+                <input class="login-input" type="text" name="username">
 
-            <label class="label" for="password">Kodeord:</label>
-            <input class="login-input" type="password" name="password">
+                <label class="label" for="password">Kodeord:</label>
+                <input class="login-input" type="password" name="password">
 
-            <button class="login-submit" type="submit" name="submit">Login</button>
-        </form>
-        <?php
-            if ( isset( $_POST["submit"] ) ) {
+                <button class="login-submit" type="submit" name="submit">Login</button>
+            </form>
+            <?php
+                if ( isset( $_POST["submit"] ) || isset( $_SESSION["id"] ) ) {
 
-                try {
-                    $login->login( $_POST["username"], $_POST["password"] );
+                    try {
+                        $login->login( $_POST["username"], $_POST["password"] );
+
+                        echo '<div class="login-message">
+                            <p class="login-message-title news-article-title">Success!:</p>
+                            <p class="error-message-text news-article-text">Velkommen ' . $_SESSION["name"] . '!</p>
+                        </div>';
+
+                        require_once 'includes/logout.inc.php';
+
+                    }
+                    catch ( Exception $e ) {
+
+                        echo '<div class="error-message">
+                            <p class="error-message-title news-article-title">ERROR:</p>
+                            <p class="error-message-text news-article-text">' . $e->getMessage() . '</p>
+                        </div>';
+
+                    }
+
                 }
-                catch ( Exception $e ) {
-
-                    echo '<div class="error-message">
-                        <p class="error-message-title">ERROR:</p>
-                        <p class="error-message-text">' . $e->getMessage() . '</p>
-                    </div>';
-
-                }
-
-            }
-        ?>
+            ?>
+        </div>
     </section>
 </main>
 
