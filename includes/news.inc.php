907f8be773aee5884e0723705885640a17b8bd06
Rasmus Kjems Knattrup <rasm355k@tietgen.dk>
2021-02-09 13:56:23 +0100

    [feat] news printer (WIP)

--- includes/news.inc.php
+++ includes/news.inc.php
@@ -1,8 +1,88 @@
-<?php require_once 'header.php'; ?>
+<?php
+// class DBH(Database handler)
+require_once 'dbconn.inc.php';
 
-<!-- MAIN -->
-<main class="main">
-    
-</main>
+class News extends DBH {
 
-<?php require_once 'footer.php'; ?>
+    // Properties
+    private $conn;
+    private $news_date;
+    private $news_date_day;
+    private $news_date_month;
+
+    // Methods
+    function __construct() {
+
+        $this->conn = $this->db_connect();
+
+    }
+
+    private function get_date( $date ) {
+
+        $this->news_date_day = substr( $date, 8, -9 );
+
+        $this->news_date_month = substr( $date, 5, -12 );
+        switch ($this->news_date_month) {
+            case '01': 
+                $this->news_date_month = "JAN";
+                break;
+            case '02': 
+                $this->news_date_month = "FEB";
+                break;
+            case '03': 
+                $this->news_date_month = "MAR";
+                break;
+            case '04': 
+                $this->news_date_month = "APR";
+                break;
+            case '05': 
+                $this->news_date_month = "MAY";
+                break;
+            case '06': 
+                $this->news_date_month = "JUN";
+                break;
+            case '07': 
+                $this->news_date_month = "JUL";
+                break;
+            case '08': 
+                $this->news_date_month = "AUG";
+                break;
+            case '09': 
+                $this->news_date_month = "SEP";
+                break;
+            case '10': 
+                $this->news_date_month = "OCT";
+                break;
+            case '11': 
+                $this->news_date_month = "NOV";
+                break;
+            case '12': 
+                $this->news_date_month = "DEC";
+                break;
+        }
+
+        $this->news_date = $this->news_date_day . ' ' . $this->news_date_month;
+
+        return $this->news_date;
+
+    }
+
+    public function print_news() {
+
+        $this->sql = 'SELECT * FROM news';
+        $this->stmt = $this->conn->prepare( $this->sql );
+        $this->stmt->execute();
+
+        $this->row = $this->stmt->fetchAll();
+
+        $this->row->date = $this->get_date( $this->row->date );
+            
+        return $this->row;
+
+    }
+
+    /*public function insert_news() {
+
+    }*/
+
+}
--- news.php
+++ news.php
@@ -1,8 +1,35 @@
-<?php require_once 'header.php'; ?>
+<?php 
+    // Header
+    require_once 'header.php';
+    // Login class
+    require_once 'includes/news.inc.php';
+?>
 
 <!-- MAIN -->
 <main class="main">
+    <!-- OVERVIEW OF LATEST NEWS -->
     <section class="news-overview">
+        
+        <?php
+            $news = new News;
+            $rows = $news->print_news();
+
+            foreach ( $rows as $row ) {
+                echo '<div class="news-card">
+                    <span class="news-overview-date">' . $row->date . '</span>
+                    <h2 class="news-article-title">' . $row->title . '</h2>
+                    <p class="news-article-text">
+                        ' . $row->content . '
+                    </p>
+                    <div class="news-read_more-wrapper margin-g">
+                        <a class="news-latest-article-read_more" href="#">LÆS MERE</a>
+                    </div>
+                </div>';
+            }
+        ?>
+
+        <!-- PLACEHOLDERS -->
+
         <div class="news-card">
             <span class="news-overview-date">11 MAR</span>
             <h2 class="news-article-title">Need For Speed</h2>
@@ -22,6 +49,68 @@
                 Hver tirsdag tilbyder Odense Track en unik og
                 sikker træningsaften, for cyklister og motionister. 
                 Vi kalder denne aften for “Banen Rundt”...
+            </p>
+            <div class="news-read_more-wrapper margin-g">
+                <a class="news-latest-article-read_more" href="#">LÆS MERE</a>
+            </div>
+        </div>
+        <div class="news-card">
+            <span class="news-overview-date">13 MAR</span>
+            <h2 class="news-article-title">DM i Gokart</h2>
+            <p class="news-article-text">
+                Danmarksmesterskaber i Gokart, afholdes i år,
+                på Odense Track. Deltagerne kommer fra hele
+                landet, niveauet er helt i top, når kørerne...
+            </p>
+            <div class="news-read_more-wrapper margin-g">
+                <a class="news-latest-article-read_more" href="#">LÆS MERE</a>
+            </div>
+        </div>
+
+
+        <div class="news-card">
+            <span class="news-overview-date">13 MAR</span>
+            <h2 class="news-article-title">DM i Gokart</h2>
+            <p class="news-article-text">
+                Danmarksmesterskaber i Gokart, afholdes i år,
+                på Odense Track. Deltagerne kommer fra hele
+                landet, niveauet er helt i top, når kørerne...
+            </p>
+            <div class="news-read_more-wrapper margin-g">
+                <a class="news-latest-article-read_more" href="#">LÆS MERE</a>
+            </div>
+        </div>
+        <div class="news-card">
+            <span class="news-overview-date">13 MAR</span>
+            <h2 class="news-article-title">DM i Gokart</h2>
+            <p class="news-article-text">
+                Danmarksmesterskaber i Gokart, afholdes i år,
+                på Odense Track. Deltagerne kommer fra hele
+                landet, niveauet er helt i top, når kørerne...
+            </p>
+            <div class="news-read_more-wrapper margin-g">
+                <a class="news-latest-article-read_more" href="#">LÆS MERE</a>
+            </div>
+        </div>
+        <div class="news-card">
+            <span class="news-overview-date">13 MAR</span>
+            <h2 class="news-article-title">DM i Gokart</h2>
+            <p class="news-article-text">
+                Danmarksmesterskaber i Gokart, afholdes i år,
+                på Odense Track. Deltagerne kommer fra hele
+                landet, niveauet er helt i top, når kørerne...
+            </p>
+            <div class="news-read_more-wrapper margin-g">
+                <a class="news-latest-article-read_more" href="#">LÆS MERE</a>
+            </div>
+        </div>
+        <div class="news-card">
+            <span class="news-overview-date">13 MAR</span>
+            <h2 class="news-article-title">DM i Gokart</h2>
+            <p class="news-article-text">
+                Danmarksmesterskaber i Gokart, afholdes i år,
+                på Odense Track. Deltagerne kommer fra hele
+                landet, niveauet er helt i top, når kørerne...
             </p>
             <div class="news-read_more-wrapper margin-g">
                 <a class="news-latest-article-read_more" href="#">LÆS MERE</a>
